eda4346454973eadcd9615d274827052bd6ddc3e
Cliff Cunnington <cliff@10strata.com>
2023-12-13 01:56:43 +0100

    play: Overlay styling and layout

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -7,29 +7,37 @@ import Image from "next/image";
 const Overlay = () => {
   return (
     <div>
-      <div className="fixed left-[5vw]  top-[5vh]  z-[1000] m-auto h-[95vh] w-[90vw] cursor-pointer rounded-2xl bg-gray-400/40 backdrop-blur-sm" />
-      <div className="fixed left-[10vw] top-[10vh] z-[1001] m-auto  h-[95vh] w-[80vw] rounded-2xl bg-white/70 p-10  ">
-        <div className="flex flex-col items-center gap-6 text-center">
-          <h6 className="border-1 small-caps text-4xl">We Style Stuff</h6>
-          <div className="flex flex-col items-center">
-            <p className="mb-12 animate-pulse text-2xl">
-              Launching in December 2023
+      <div className="fixed  left-[0vh] top-[2vh]  z-[1000] m-auto h-[98vh] w-[100vw] cursor-pointer rounded-2xl bg-zinc-200/10 backdrop-blur-sm" />
+      <div className="fixed left-[10vw] top-[10vh] z-[1002] m-auto h-[360px] w-[480px]  rounded-2xl bg-white/90 p-10  ">
+        <div className="z-[1001] -mt-10 ml-2 flex -rotate-6 flex-col items-center gap-6 text-center">
+          <h6 className="border-1 small-caps text-4xl text-zinc-800 ">
+            We Style Stuff.
+          </h6>
+          <div className="flex flex-col items-center ">
+            <p className="mb-7 text-lg font-semibold text-zinc-800/40">
+              <span className="small-caps align-sup text-4xl text-teal-600/70">
+                We
+              </span>{" "}
+              will be launching in December 2023
             </p>
-            <QuoteCard
-              imgSrc={martyImg}
-              attribution="Marty Neumeier"
-              smHandle="martyneumeier.com"
-            >
-              Brand is not what you say it is. It&apos;s what they say it is.
-            </QuoteCard>
+
             <Image
               src={ourLogoImg}
               alt="We Style Stuff graphic"
               height={240}
               width={240}
-              className="mt-4 rounded-full shadow-lg outline-4 ring-4 ring-gray-300/40 ring-offset-2"
+              className="mt-2 rounded-full shadow-lg outline-4 ring-4 ring-gray-300/40 ring-offset-2"
             />
           </div>
+        </div>
+        <div className="fixed bottom-10 right-10 z-[999] rotate-3">
+          <QuoteCard
+            imgSrc={martyImg}
+            attribution="Marty Neumeier"
+            smHandle="Author of The Brand Flip"
+          >
+            Brand is not what you say it is. It&apos;s what they say it is.
+          </QuoteCard>
         </div>
       </div>
     </div>
--- src/components/quote-card.tsx
+++ src/components/quote-card.tsx
@@ -16,26 +16,34 @@ const QuoteCard = ({
   imgSrc,
 }: QuoteCardProps) => {
   return (
-    <div className="mx-auto w-full max-w-[500px] rounded-lg bg-white px-5 pb-10 pt-5 text-gray-800 shadow-lg">
+    <div className="mx-auto w-full max-w-[500px] rounded-lg bg-white/90 px-5 pb-12 pt-5 text-gray-800 shadow-lg">
       <div className="w-full pb-5 pt-1">
-        <div className="ring-3 mx-auto -mt-16 h-20 w-20 overflow-hidden rounded-full shadow-lg outline-4 ring-1  ring-gray-400  ">
-          <Image src={imgSrc} alt="{attribution}" width={128} height={128} />
+        <div className="mx-auto -mt-10 h-20 w-20 overflow-hidden rounded-full shadow-lg outline-4 ring-2 ring-zinc-800/30  ring-offset-2  ">
+          <Image
+            src={imgSrc}
+            alt="{attribution}"
+            width={128}
+            height={128}
+            className="-rotate-[10deg] sepia-[.7]"
+          />
         </div>
       </div>
       <div className="mb-10 w-full">
-        <div className="h-3 text-left text-3xl leading-tight text-indigo-500">
+        <div className="h-3 text-left text-3xl leading-tight text-teal-600">
           “
         </div>
-        <p className="px-5 text-center text-sm text-gray-600">{children}</p>
-        <div className="-mt-3 h-3 text-right text-3xl leading-tight text-indigo-500">
+        <p className="small-caps px-5 text-center text-sm text-zinc-600">
+          {children}
+        </p>
+        <div className="-mt-3 h-3 text-right text-3xl leading-tight text-teal-600">
           ”
         </div>
       </div>
       <div className="w-full">
-        <p className="text-md text-center font-bold text-indigo-500">
+        <p className="text-md text-center font-semibold text-teal-600">
           {attribution}
         </p>
-        <p className="text-center text-xs text-gray-500">{smHandle}</p>
+        <p className="text-center text-xs text-zinc-500">{smHandle}</p>
       </div>
     </div>
   );
